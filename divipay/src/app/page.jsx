5ba9ca1fcb291ea4437bc878503a3e0288c444a3
aryannaik225 <aryann2203@gmail.com>
2025-02-18 22:14:34 +0530

    added the functionality to change currency

--- divipay/src/app/page.jsx
+++ divipay/src/app/page.jsx
@@ -6,6 +6,14 @@ import { useState, useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const currencyList = [
+  { name: "Indian Rupee", symbol: "₹" },
+  { name: "Euro", symbol: "€" },
+  { name: "Belize Dollar", symbol: "BZ$" },
+  { name: "Hong Kong Dollar", symbol: "HK$" },
+  { name: "Pakistani Rupee", symbol: "Rs" },
+];
+
 export default function Home() {
 
   const [nameInput, setNameInput] = useState("")
@@ -21,6 +29,14 @@ export default function Home() {
   const [discountInput, setDiscountInput] = useState("");
   const [discountSymbol, setDiscountSymbol] = useState("%");
   const [discounts, setDiscounts] = useState([]);
+  const [taxInput, setTaxInput] = useState("");
+  const [taxSymbol, setTaxSymbol] = useState("%");
+  const [taxes, setTaxes] = useState([]);
+  const [taxName, setTaxName] = useState("");
+
+  const [currencyInput, setCurrencyInput] = useState("");
+  const [filteredCurrencies, setFilteredCurrencies] = useState(currencyList);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme") || "light";
@@ -118,6 +134,54 @@ export default function Home() {
     }
   }
 
+  const removeDiscount = (index) => {
+    setDiscounts(discounts.filter((_, i) => i !== index));
+  }
+
+  const addTax = () => {
+    if (taxInput.trim() && taxName.trim()) {
+      setTaxes([...taxes, { name: taxName, value: parseFloat(taxInput), symbol: taxSymbol }]);
+      setTaxInput("");
+      setTaxName("");
+    } else if (!taxName.trim()) {
+      toast.error("Tax name cannot be empty", {
+        toastId: "taxNameEmptyWarning",
+      });
+    } else if (!taxInput.trim()) {
+      toast.error("Tax value cannot be empty", {
+        toastId: "taxEmptyWarning",
+      });
+    }
+  }
+
+  const removeTax = (index) => {
+    setTaxes(taxes.filter((_, i) => i !== index));
+  }
+
+  const handleCurrencyInputChange = (e) => {
+    const value = e.target.value;
+    setCurrencyInput(value);
+
+    if (value === "") {
+      setFilteredCurrencies(currencyList);
+    } else {
+      setFilteredCurrencies(
+        currencyList.filter(
+          (currency) =>
+            currency.name.toLowerCase().includes(value.toLowerCase()) ||
+            currency.symbol.toLowerCase().includes(value.toLowerCase())
+        )
+      );
+    }
+    setShowDropdown(true);
+  };
+
+  const handleCurrencySelect = (currency) => {
+    setCurrencyInput(`${currency.name} (${currency.symbol})`);
+    setShowDropdown(false);
+  };
+
+
   if (theme === null) return null;
 
   return (
@@ -204,7 +268,7 @@ export default function Home() {
             value={costInput}
             onChange={(e) => setCostInput(e.target.value)}
             placeholder="Cost"
-            className="w-full p-2 mb-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular"
+            className="w-full p-2 mb-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular no-arrows"
           />
 
           <div className="flex flex-wrap gap-2 mb-3 w-full bg-[#e5e5e5] dark:bg-[#2f2f2f] p-2 rounded min-h-[50px]">
@@ -288,14 +352,111 @@ export default function Home() {
         </div>
 
         {/* Displaying the added discounts */}
-        <div className="mt-4">
+        <div className="mt-4 flex gap-2 flex-wrap">
           {discounts.map((discount, index) => (
-            <div key={index} className="flex items-center justify-between p-3 bg-[#d9d9d9] dark:bg-gray-700 rounded mb-2">
+            <div key={index} className="flex items-center justify-between p-3 bg-[#d9d9d9] dark:bg-gray-700 rounded mb-2 gap-4">
               <p className="text-[#1f1f1f] dark:text-white">
-                <b>{discount.value}{discount.symbol}</b>
+                {discount.value}{discount.symbol} Discount
               </p>
+              <button
+                type="button"
+                onClick={() => removeDiscount(index)}
+                className="text-red-500"
+              >
+                ✕
+              </button>
             </div>
           ))}
+        </div>
+
+        {/* Taxes Input */}
+        <div className="w-full mt-10">
+          <label className="block text-[#1f1f1f] dark:text-white mb-1 poppins-regular">
+            Add Taxes
+          </label>
+
+          <div className="w-full flex gap-[6px]">
+            <input 
+              type="text" 
+              value={taxName}
+              onChange={(e) => setTaxName(e.target.value)}
+              placeholder="Tax Name"
+              className="w-full p-2 mb-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular"
+            />
+            <input 
+              type="number"
+              value={taxInput}
+              onChange={(e) => setTaxInput(e.target.value)}
+              placeholder="Tax"
+              className="w-2/6 p-2 mb-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular no-arrows"
+            />
+            <select
+              value={taxSymbol}
+              onChange={(e) => setTaxSymbol(e.target.value)}
+              className="w-1/3 p-2 mb-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular appearance-none"
+            >
+              <option value="%">%</option>
+              <option value="₹">₹</option>
+            </select>
+          </div>
+
+          <button
+            type="button"
+            onClick={addTax}
+            className="mt-2 p-2 w-full bg-[#e5e5e5] dark:bg-[#2f2f2f] dark:hover:bg-[#1f1f1f] text-white rounded"
+          >
+            Add Tax
+          </button>
+        </div>
+
+        {/* Displaying the added taxes */}
+        <div className="mt-4 flex gap-2 flex-wrap">
+          {taxes.map((tax, index) => (
+            <div key={index} className="flex items-center justify-between p-3 bg-[#d9d9d9] dark:bg-gray-700 rounded mb-2 gap-4">
+              <p className="text-[#1f1f1f] dark:text-white">
+                {tax.name} - {tax.value}{tax.symbol} Tax
+              </p>
+              <button
+                type="button"
+                onClick={() => removeTax(index)}
+                className="text-red-500"
+              >
+                ✕
+              </button>
+            </div>
+          ))}
+        </div>
+
+
+        {/* Currency Input */}
+        <div className="w-full mt-10">
+          <label className="block text-[#1f1f1f] dark:text-white mb-1 poppins-regular">
+            Select Currency
+          </label>
+
+          <input 
+            type="text" 
+            value={currencyInput}
+            onChange={handleCurrencyInputChange}
+            onFocus={() => setShowDropdown(true)}
+            placeholder="Type or select a currency"
+            className="w-full p-2 rounded bg-[#e5e5e5] dark:bg-[#2f2f2f] text-[#1f1f1f] dark:text-white poppins-regular"
+          />
+
+          {showDropdown && (
+            <ul className="absolute w-[400px] mt-1 max-h-40 overflow-auto bg-[#e5e5e5] dark:bg-[#2f2f2f] rounded shadow-lg">
+              {filteredCurrencies.map((currency, index) => (
+                <li
+                  key={index}
+                  onClick={() => handleCurrencySelect(currency)}
+                  className="p-2 cursor-pointer hover:bg-blue-400 dark:hover:bg-blue-400 text-[#1f1f1f] dark:text-white transition-colors"
+                >
+                  {currency.name} ({currency.symbol})
+                </li>
+              ))}
+            </ul>
+          )}
+
         </div>
 
       </form>
