a65db5c29360371653e760422026850c724ef465
aryannaik225 <aryann2203@gmail.com>
2026-01-25 18:30:02 +0530

    upgraded all the dependencies

--- divipay/next.config.mjs
+++ divipay/next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
 
-import nextPwa from 'next-pwa';
-
-const withPWA = nextPwa({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
-
-
-const nextConfig = nextPwa({
-  experimental: {
-    appDir: true,
-  },
-});
-
-export default withPWA(nextConfig);
-
-
+export default nextConfig;
--- divipay/src/app/layout.jsx
+++ divipay/src/app/layout.jsx
@@ -1,34 +1,36 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import ServiceWorkerRegister from "@/utils/ServiceWorkerRegister";
 import PWAInstallPrompt from "@/components/PWAInstallPrompt";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
+  weight: [
+    "100", "200", "300", "400", "500",
+    "600", "700", "800", "900"
+  ],
+  style: ["normal", "italic"],
+  variable: "--font-poppins",
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const viewport = { 
+  themeColor: "#ffffff",
+}
 
 export const metadata = {
   title: "DiviPay",
   description: "An app to simplify expense distribution among friends",
-  themeColor: "#ffffff",
   manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         {children}
         <ServiceWorkerRegister />
